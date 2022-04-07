50c1faa10036a4cf42811203ee924fc8b4099f38
Pranay Ashok Ramteke <69661033+Phoenix600@users.noreply.github.com>
2022-04-07 15:56:29 +0530

    Python coding started

---
+++ 01_hello.py
@@ -0,0 +1,5 @@
+# This is how we print the string in python
+# we use print function in python
+print("Lorem Ipsum\n")
+print("Hello python interpreter\n")
+print("Welcome to the 100 days of code in python bootcamp\n")
