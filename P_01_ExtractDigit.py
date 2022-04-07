c068d7a6f8a358f767e3e4139e0dfc1be6923381
Pranay Ashok Ramteke <69661033+Phoenix600@users.noreply.github.com>
2022-04-07 16:05:50 +0530

    Extraction of digit problem solved

---
+++ P_01_ExtractDigit.py
@@ -0,0 +1,8 @@
+#### problem 
+### Input  : 123456
+#### Output : 1+2+3 
+#### Limit using only two variables or an arrays of string 
+
+
+string_num = input()
+print(int(string_num[0])+ int(string_num[1]) + int(string_num[2]))
